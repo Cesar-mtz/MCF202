da88beb59a325b4ef8d43d947a6a910136d01bf7
Cesar-mtz <aldair-mtz@hotmail.com>
2019-08-05 15:28:57 -0500

    Script 0

---
+++ Clase 0/Clase_0.R
@@ -0,0 +1,27 @@
+# Cesar Martinez Gauna
+# 05/08/2019
+# Clase 0
+
+
+# Pasos básicos -----------------------------------------------------------
+
+2 + 2
+a <- 2
+a + a
+a + 5
+
+# Importar datos ----------------------------------------------------------
+
+
+diametro <- c(12, 8.6, 9.2, 7.7, 12.9, 11.7, 9.7, 14.2,
+              11.8, 14.3, 12.5)
+
+diametro
+# Medidas de tendencia central
+mean(diametro)
+median(diametro)
+
+#MEdias de dispersión
+sd(diametro)
+var(diametro)
+
